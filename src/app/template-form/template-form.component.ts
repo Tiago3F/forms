088add88112ad7688869036addefa503fd47453a
tiago <tiago.moreira@timebrisa.com.br>
2022-04-28 16:51:48 -0300

    feat: :sparkles: Validação de inputs[

--- src/app/template-form/template-form.component.ts
+++ src/app/template-form/template-form.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class TemplateFormComponent implements OnInit {
 
   usuario: any = {
-    nome: 'Tiago',
-    email: 'tiago@gmail.com'
+    nome: null,
+    email: null
   }
 
   constructor() { }
