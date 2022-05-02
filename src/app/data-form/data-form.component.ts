e19a7c3ae18f82348ed2c52a8e159f3506a292d0
tiago <tiago.moreira@timebrisa.com.br>
2022-05-02 13:31:39 -0300

    feat: :sparkles: Consultando CEP

--- src/app/data-form/data-form.component.ts
+++ src/app/data-form/data-form.component.ts
@@ -73,4 +73,65 @@ export class DataFormComponent implements OnInit {
     }
   }
 
+  consultaCEP() {
+
+    let cep = this.formulario.get('endereco.cep')?.value
+
+    cep = cep.replace(/\D/g, '');
+
+    if (cep != null && cep !== '') {
+      const validacep = /^[0-9]{8}$/;
+
+      if (validacep.test(cep)) {
+
+        this.resetaDadosForm()
+
+        this.http.get(`//viacep.com.br/ws/${cep}/json/`)
+          .pipe(map((dados: any) => dados))
+          .subscribe(dados => this.populaDadosForm(dados));
+      }
+    }
+  }
+
+  populaDadosForm(dados: any) {
+    // formulario.setValue({
+    //   nome: formulario.value.nome,
+    //   email: formulario.value.email,
+    //   endereco: {
+    //     rua: dados.logradouro,
+    //     cep: dados.cep,
+    //     numero: '',
+    //     complemento: dados.complemento,
+    //     bairro: dados.bairro,
+    //     cidade: dados.localidade,
+    //     estado: dados.uf
+    //   }
+    // })
+
+    this.formulario.patchValue({
+      endereco: {
+        rua: dados.logradouro,
+        cep: dados.cep,
+        complemento: dados.complemento,
+        bairro: dados.bairro,
+        cidade: dados.localidade,
+        estado: dados.uf
+      }
+    })
+
+  }
+
+  resetaDadosForm() {
+    this.formulario.patchValue({
+      endereco: {
+        rua: null,
+        cep: null,
+        complemento: null,
+        bairro: null,
+        cidade: null,
+        estado: null
+      }
+    })
+  }
+
 }
